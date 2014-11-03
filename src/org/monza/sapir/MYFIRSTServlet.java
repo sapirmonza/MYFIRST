b5773b41334c269bfe9238496f7fb249a8de9dd4
sapir monza <sapirmonza@gmail.com>
2014-11-03 22:28:08 +0200

    result 49 works

--- src/org/monza/sapir/MYFIRSTServlet.java
+++ src/org/monza/sapir/MYFIRSTServlet.java
@@ -5,9 +5,10 @@ import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class MYFIRSTServlet extends HttpServlet {
+	int num1=3, num2=4, num3=7;
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, sapir monza");
+		resp.getWriter().printf("Result of (%d+%d)*%d=%d",num1,num2,num3,(num1+num2)*num3);
 	}
 }
